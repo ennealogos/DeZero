c5acfc0265751bf003389621bae7146a66f1a7ba
ennealogos <ennealogos@outlook.com>
2024-11-28 22:05:09 +0800

    finished step 19

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import unittest
 import numpy as np
 import weakref
+import contextlib
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name = None):
         # 仅支持ndarray
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
+        self.name = name
         self.data = data
         self.grad = None
         self.creator = None
         self.generation = 0 # 用于确定反向传播的顺序
+
+    # 返回变量形状，且让方法像变量的属性一样易用
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    # 维度数
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    # 元素个数
+    @property
+    def size(self):
+        return self.data.size
+
+    # 数据类型
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    # 使得len(Varable)可用
+    def __len__(self):
+        return len(self.data)
+    
+    # 使得print()能用于Varable
+    def __repr__(self):
+        if self.data is None:
+            return 'Varable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 8) # 保证多行输出时对齐
+        return 'Varable(' + p + ')'
 
     # 设定生成函数
     def set_creator(self, func):
@@ -24,7 +57,7 @@ class Variable:
         self.grad = None
 
     # 反向传播
-    def backward(self):
+    def backward(self, retain_grad=False): # 若为True则保留中间变量的导数
         # 若对该变量调用反向传播时，导数仍为空，则初始化导数为1.0（说明是反向传播起始点）
         if self.grad is None:
             self.grad = np.ones_like(self.data)
@@ -54,6 +87,11 @@ class Variable:
                 # 只要x还有创建者，将其创建者添加到反向传播的函数列表
                 if x.creator is not None:
                     add_func(x.creator)
+            # 清除中间变量的导数
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # y是弱引用
+            
 
 
 # 将标量转化为numpy数组
@@ -62,6 +100,25 @@ def as_array(x):
         return np.array(x)
     return x
 
+# 配置类
+class Config:
+    enable_backprop = True # 实现模式切换，只有在训练模式中才需要记录中间变量和变量与函数之间的生成关系
+
+@contextlib.contextmanager
+def using_config(name, value):
+    # 进入函数后将enable_backprop设置为value
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    # 在with作用域内执行自己想要执行的操作
+    try: 
+        yield
+    # 离开作用域时将属性复原
+    finally:
+        setattr(Config, name, old_value)
+
+# 封装模式切换 不存储中间变量导数
+def no_grad():
+    return using_config('enable_backprop', False)
 
 class Function:
     def __call__(self, *inputs): # 接受一个可变长参数
@@ -71,11 +128,13 @@ class Function:
             ys = (ys,)
         # as_array防止出现不支持的数据类型
         outputs = [Variable(as_array(y)) for y in ys]
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = [weakref.ref(output) for output in outputs]
+        # 只有在训练模式中才会记录元素之间的连接关系
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
         return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, xs):
@@ -156,6 +215,10 @@ class SquareTest(unittest.TestCase):
 # 程序测试入口
 # unittest.main()
 
-for i in range(10):
-    x = Variable(np.random.randn(10000)) # 大量数据
-    y = square(square(square(x))) # 进行复杂的计算
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+print(x.shape) # 用x.shape代替x.shape()来访问
+print(x.ndim)
+print(x.size)
+print(x.dtype)
+print(len(x))
+print(x)
