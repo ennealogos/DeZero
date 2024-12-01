06fbbe1002fa43ac70b6599e29ba79233e848295
ennealogos <ennealogos@outlook.com>
2024-12-01 21:00:27 +0800

    finished step 24 (stage 2)

--- test.py
+++ test.py
@@ -3,8 +3,26 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 from dezero import Variable
+
+
+# =========================================
+# 优化函数
+
+def sphere(x, y):
+    z = x ** 2 + y ** 2
+    return z
+
+def matyas(x, y): 
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
+
+def goldstein(x, y): 
+    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
+
 x = Variable(np.array(1.0))
-y = (x + 3) ** 2
-y.backward()
-print(y)
-print(x.grad)
+y = Variable(np.array(1.0))
+z = goldstein(x, y)
+z.backward()
+print(x.grad, y.grad)
